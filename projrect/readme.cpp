5e5462f195126a166026763d93d80b2e5cf3807b
densishankar-arch <densishankar@gmail.com>
2026-04-12 17:19:04 +0530

    Update and rename README.md to projrect/readme.cpp

---
+++ projrect/readme.cpp
@@ -0,0 +1,15 @@
+# C++ Calculator
+
+This is a simple calculator program written in C++.
+
+## Features
+- Addition
+- Subtraction
+- Multiplication
+- Division
+
+## How to Run
+1. Compile the code:
+   g++ calculator.cpp -o calculator
+2. Run the program:
+   ./calculator
